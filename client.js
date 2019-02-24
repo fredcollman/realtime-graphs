521148d17a220166d07d0bf6b8e0527dbe454fc7
Fred Collman <fredcollman@gmail.com>
2019-02-24 18:51:04 +0000

    Attempt to convert to pure SVG

--- client.js
+++ client.js
@@ -1,11 +1,18 @@
-const ws = new WebSocket("ws://127.0.0.1:8765/");
+const MARGIN = { top: 10, right: 10, bottom: 20, left: 50 }
+const OUTER = { width: 800, height: 300}
+const INNER = { 
+  width: OUTER.width - MARGIN.left - MARGIN.right,
+  height: OUTER.height - MARGIN.top - MARGIN.bottom,
+};
+const BARS = ["min", "max", "avg", "cur"]
+
 let state = {
   total: 0,
   count: 0,
   min: 1,
   max: 0,
   latest: 0,
-}
+};
 
 const reducer = (state, data) => ({
   total: state.total + data,
@@ -13,22 +20,68 @@ const reducer = (state, data) => ({
   min: Math.min(state.min, data),
   max: Math.max(state.max, data),
   latest: data,
-})
+});
 
-ws.onmessage = (event) => {
-  state = reducer(state, parseFloat(event.data));
-  // console.log(state);
+const xScale = d3.scaleLinear()
+  .domain([0, 1])
+  .range([0, INNER.width])
 
-  const bars = d3.select('#app')
-    .selectAll('div')
-    .data([state.min, state.max, state.total/state.count, state.latest])
+const yScale = d3.scaleBand()
+  .domain(BARS)
+  .range([0, INNER.height])
+
+const render = state => {
+  const bars = d3.select('svg')
+    .selectAll('g rect')
+    .data([state.min, state.max, state.total/state.count, state.latest]);
 
   const newBars = bars.enter()
-    .append('div')
+    .append('g')
+    .append('rect')
     .attr('class', 'bar')
-    .style('width', 0)
+    .attr('width', 0)
+    .attr('height', d => yScale.bandwidth())
+    .attr('y', (d, i) => i * yScale.bandwidth());
 
   newBars.merge(bars)
     .transition()
-    .style('width', d => `${d * 100}%`)
-};
+    .attr('width', d => xScale(d));
+}
+
+const handle = event => {
+  state = reducer(state, parseFloat(event.data));
+  // console.log(state);
+  render(state);
+}
+
+const addStopButton = ({ws}) => {
+  const button = document.createElement("button");
+  button.innerText = "Stop";
+  button.onclick = () => ws.close();
+  document.body.appendChild(button);
+}
+
+const addChart = () => {
+  const svg = d3.select('#app')
+    .append('svg')
+    .attr('width', OUTER.width)
+    .attr('height', OUTER.height)
+
+  const axisContainer = svg.append('g')
+    .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`);
+
+  svg.append('g')
+    .attr('transform', `translate(0, ${INNER.height})`)
+    .call(d3.axisBottom(xScale))
+  svg.append('g').call(d3.axisLeft(yScale))
+}
+
+const init = () => {
+  const app = document.querySelector('#app')
+  const ws = new WebSocket("ws://127.0.0.1:8765/");
+  ws.onmessage = handle;
+  addChart()
+  addStopButton({ws, app});
+}
+
+init();
